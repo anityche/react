514839b49c157ba22fc448fe2a8711d9c304ddef
srec0051 <srec0051@sorincorp.com>
2024-03-05 11:26:03 +0900

    R026 설명추가

--- client/src/component/R026_ForceUpdate.js
+++ client/src/component/R026_ForceUpdate.js
@@ -4,6 +4,7 @@ import React, {Component} from "react";
  * state 직접 변경 후 forceUpdate() 함수 사용
  * 1. button의 onClick 이벤트로 this.state.변수명=value 문법으로 
  * state를 직접 변경한다. StateString 값은 '리액트'로 변경된다.
+ * 2. forceUpdate() 함수는 화면을 강제로 새로 고침하기 때문에 render() 함수를 실행시킨다.
  */
 class R026_ForceUpdate extends Component {
 	constructor (props) {
