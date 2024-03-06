4199385decad8b7b2879cd18d733f3f0708065a4
srec0051 <srec0051@sorincorp.com>
2024-03-06 16:04:28 +0900

    R029 설명 추가

--- client/src/component/R029_ShallowEqual.js
+++ client/src/component/R029_ShallowEqual.js
@@ -11,6 +11,8 @@ import { shallowEqualArrays} from 'shallow-equal';
  * 2. shallow-equal package는 PureComponent에서 state 값의 변경을 비교하는 것과 동일한 기능의 함수를 제공한다.
  * 3. shallowEqualArrays() 함수를 사용하면 문자열과 배열은 값만 비교한다.
  * 4. 객체는 PureComponent와 동일하게 참조 값을 비교한다. 
+ * 5. component 클래스에서도 shouldComponentUpdate(), shallowEqualArrays() 함수를 사용하면 
+ * 	PureComponent 클래스처럼 값만 비교해 render() 함수를 실행시킬 수 있다.
  */
 class R029_ShallowEqual extends Component {
 	constructor (props) {
@@ -19,8 +21,10 @@ class R029_ShallowEqual extends Component {
 		this.state = { StateString: 'react' }
 	}
 
+	// 이 함수는 변경된 props와 state 값을 파라미터로 받을 수 있고 반환 값이 true일 때 render() 함수를 실행한다.
 	shouldComponentUpdate (nextProps, nextState){
-		return !shallowEqualArrays(this.state, nextState)
+		console.log('shouldComponentUpdate call');
+		return !shallowEqualArrays(this.state, nextState);
 	}
 
 	componentDidMount(){
@@ -29,8 +33,11 @@ class R029_ShallowEqual extends Component {
 		const Array2 = ['리액트', object];
 		const Array3 = ['리액트', { react : '200'}];
 
-		console.log('shallowEqualArrays (Array1, Array2): ' + shallowEqualArrays (Array1, Array2)); 
-		console.log('shallowEqualArrays (Array2, Array3): ' + shallowEqualArrays (Array2, Array3));
+		/**
+		 * array2와 array3을 비교하면 값은 같지만, 다른 참조 값을 갖는 다른 객체로 판단한다.
+		 */
+		console.log('shallowEqualArrays (Array1, Array2): ' + shallowEqualArrays (Array1, Array2)); // true
+		console.log('shallowEqualArrays (Array2, Array3): ' + shallowEqualArrays (Array2, Array3)); // false
 		
 		this.setState({StateString : 'react'});
 	}
