5620d57f308238fb8009b0b92ca553231d8768cb
anityche <anityche@gmail.com>
2024-04-05 23:19:20 +0900

    clone disney app - 이미지 배너 UI 생성하기

--- react-disney-app/src/components/Banner.js
+++ react-disney-app/src/components/Banner.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
+import styled from 'styled-components';
 import axios from '../api/axios'
 import requests from '../api/request';
+import "./Banner.css";
 
 const Banner = () => {
 	const [movie, setMovie] = useState([]);
@@ -24,13 +26,75 @@ const Banner = () => {
 		const {data: movieDetail } = await axios.get(`/movie/${movieId}`,{ 
 			params: {append_to_response: 'videos'}
 		});
-		console.log('movieDetail', movieDetail);
+		// console.log('movieDetail', movieDetail);
 		setMovie(movieDetail);
 	}
 
+	// 지정된 문자열 개수보다 많으면 '...' 표시로 
+	const truncate = (str, n) => {
+		return str?.length > n ? str.substring(0, n) + "..." : str;
+	}
+
 	return (
-		<div>Banner</div>
+		<header 
+			className='banner'
+			style={{
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+				backgroundPosition: 'top center',
+				backgroundSize: 'cover'
+			}}
+		>
+			<div className='banner__contents'>
+				<h1 className='banner__title'>
+					{ movie.title || movie.name || movie.original_name }
+				</h1>
+
+				<div className='banner__buttons'>
+					{ movie?.videos?.results[0]?.key && 
+						<button className='banner__button play'>
+						Play
+						</button>
+					}
+				</div>
+
+				<p className='banner__desciption'>
+					{ truncate(movie.overview, 100) }
+				</p>
+			</div>
+			<div className='banner--fadeBottom'  />
+		</header>
 	)
 }
 
 export default Banner
+
+// const Container = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   width: 100%;
+//   height: 100vh;
+// `;
+
+// const HomeContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+// `;
+
+// const Iframe = styled.iframe`
+//   width: 100%;
+//   height: 100%;
+//   z-index: -1; 
+//   opacity: 0.65;
+//   border: none;
+
+//   &::after {
+//     content: "";
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//   }
+// `;
